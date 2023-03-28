3bc8f0068dcf164fb28f3f9ab21ec95c801ddcf9
Vitor Nunes <stecks10@gmail.com>
2023-03-28 17:54:43 -0300

    metodo POST

--- src/server.js
+++ src/server.js
@@ -2,18 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.post("/users", (request, response) => {
 
-  response.send(`
-  Mensagem ID: ${id}. 
-  Para o usuario ${user}`);
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Pagina: ${page}. Mostrar: ${limit}`);
+  response.send("Voce chamou o POST");
 });
 
 const PORT = 3333;
