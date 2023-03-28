67119f7e9ffd3d4ba40072a025cd0d5f32e54834
Vitor Nunes <stecks10@gmail.com>
2023-03-28 18:01:02 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
 app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send("Voce chamou o POST");
+  response.json({ name, email, password });
 });
 
 const PORT = 3333;
