98044ba022102ac40ecc8ba6e0fb2761003e3fba
Vitor Nunes <stecks10@gmail.com>
2023-03-28 17:16:53 -0300

    queryparams

--- src/server.js
+++ src/server.js
@@ -8,7 +8,14 @@ app.get("/message/:id/:user", (request, response) => {
   response.send(`
   Mensagem ID: ${id}. 
   Para o usuario ${user}`);
-})
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Pagina: ${page}. Mostrar: ${limit}`);
+});
 
 const PORT = 3333;
+
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
