90ea5f3cae82f02aa4bacef3bdeb3879db86b6c3
Vitor Nunes <stecks10@gmail.com>
2023-04-04 10:25:31 -0300

    AppError

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,7 +2,7 @@ class UserController {
   create(request, response) {
     const { name, email, password } = request.body;
 
-    response.json({ name, email, password });
+    response.status(201).json({ name, email, password });
   }
 }
 
