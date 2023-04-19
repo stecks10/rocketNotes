c07b315fdfa916448c539e356ca7b4aacf923a0f
Vitor Nunes <stecks10@gmail.com>
2023-04-19 15:06:00 -0300

    1

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -22,6 +22,38 @@ class UserController {
 
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const userWithUpdateEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError('E-mail already exists');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      update_at = ?,
+      where id = ?`,
+      [user.name, user.email, new Date(), user.id]
+    );
+
+    return response.json();
+  }
 }
 
 module.exports = UserController;
