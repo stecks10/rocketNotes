004d59893857baf35609c4993e2220faea2b0a78
Vitor Nunes <stecks10@gmail.com>
2023-04-23 18:35:17 -0300

    exibindo nota

--- src/database/knex/migrations/20230423144721_createTags.js
+++ src/database/knex/migrations/20230423144721_createTags.js
@@ -1,10 +1,9 @@
 exports.up = knex => knex.schema.createTable("tags", table => {
   table.increments("id");
-  table.text("name").notNullable();
+  table.text("name");
 
-  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
-  table.integer("user_id").references("id").inTable("users");
-
+  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");// quando deletar uma <note> específica deleta também as <tags> relacionadas a está <note>
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
 });
 
 exports.down = knex => knex.schema.dropTable("tags");
