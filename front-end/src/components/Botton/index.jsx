1cee13f6e4090958e8b5024d44b03cc996f27919
Vitor Nunes <stecks10@gmail.com>
2023-04-24 16:14:15 -0300

    component

---
+++ front-end/src/components/Botton/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export function Button() {
+  return (
+    <Container type='button'>
+      Meu botão
+    </Container>
+  )
+
+}
--- front-end/src/pages/Details/index.jsx
+++ front-end/src/pages/Details/index.jsx
@@ -1,9 +1,11 @@
+import { Button } from '../../components/Botton';
 import { Container } from './styles';
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
+      <Button />
     </Container>
   );
 }
