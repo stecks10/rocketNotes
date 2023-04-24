4c5350a38892b95b979f29a141330133aa908e10
Vitor Nunes <stecks10@gmail.com>
2023-04-23 21:59:26 -0300

    filtro whereIn

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -57,12 +57,23 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id, title } = request.query;
+    const { user_id, title, tags } = request.query;
 
-    const notes = await knex("notes")
-      .where({ user_id })
-      .whereLike("title", `%${title}%`)
-      .orderBy("title");
+    let notes;
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim());
+
+      notes = await knex("tags")
+        .whereIn("name", filterTags)
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
+
+
 
     return response.json(notes);
   }
