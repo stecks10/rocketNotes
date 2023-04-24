466a96334dab9dd7593b463bd3ce5bad335fff46
Vitor Nunes <stecks10@gmail.com>
2023-04-24 11:27:31 -0300

    obtendo tags da nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -82,9 +82,19 @@ class NotesController {
         .orderBy("title");
     }
 
+    const userTags = await knex("tags").where({ user_id });
+
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...notes,
+        tags: noteTags,
+      }
+    })
 
 
-    return response.json(notes);
+    return response.json(notesWithTags);
   }
 }
 
