e784ba3de31dba5c599e1413c799330c7f70eaec
Vitor Nunes <stecks10@gmail.com>
2023-04-24 16:27:05 -0300

    propriedade

--- front-end/src/pages/Details/index.jsx
+++ front-end/src/pages/Details/index.jsx
@@ -1,11 +1,12 @@
-import { Button } from '../../components/Botton';
+import { Button } from '../../components/Button';
 import { Container } from './styles';
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
-      <Button />
+      <Button title="Entrar" />
+      <Button title="Cadastrar" />
     </Container>
   );
 }
