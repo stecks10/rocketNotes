7338935fcfe8ec3b2dbd3062217333df2a9bb018
Vitor Nunes <stecks10@gmail.com>
2023-04-24 16:36:39 -0300

    component header

--- front-end/src/pages/Details/index.jsx
+++ front-end/src/pages/Details/index.jsx
@@ -1,12 +1,12 @@
 import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
 import { Container } from './styles';
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World</h1>
-      <Button title="Entrar" />
-      <Button title="Cadastrar" />
+      <Header />
+      <Button title="Voltar" />
     </Container>
   );
 }
