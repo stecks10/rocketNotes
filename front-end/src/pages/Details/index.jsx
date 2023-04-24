44143537ab97bb143fac9828db29fb65f1e84452
Vitor Nunes <stecks10@gmail.com>
2023-04-24 20:46:39 -0300

    finalizando detalhes

--- front-end/src/pages/Details/index.jsx
+++ front-end/src/pages/Details/index.jsx
@@ -3,27 +3,35 @@ import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 import { Tag } from '../../components/Tag';
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <ButtonText title="Excluir nota" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Link uteis">
-        <Links>
-          <li><a href='#'>https://www.google.com.br</a></li>
-          <li><a href='#'>https://www.google.com.br</a></li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="note" />
-      </Section>
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto quos est laboriosam, iste aut quas iure non dolore aliquid commodi ipsam porro tempore cum itaque eum dignissimos perspiciatis modi sit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sapiente dicta quo iusto, vero, quis expedita sunt sit, accusantium et consequuntur quia. Cupiditate ullam provident maiores earum assumenda omnis excepturi.</p>
 
-      <Button title="Voltar" />
+          <Section title="Link uteis">
+            <Links>
+              <li><a href='#'>https://www.google.com.br</a></li>
+              <li><a href='#'>https://www.google.com.br</a></li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="note" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
