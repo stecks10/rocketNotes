e516ac516ceba561ef2cca1d2290a0e915730ffe
Vitor Nunes <stecks10@gmail.com>
2023-04-24 11:51:59 -0300

    listando tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const usersRouter = require('./users.routes')
 const notesRoutes = require('./notes.routes')
+const tagsRoutes = require('./tags.routes')
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/notes', notesRoutes);
+routes.use('/tags', tagsRoutes);
 
 module.exports = routes;
