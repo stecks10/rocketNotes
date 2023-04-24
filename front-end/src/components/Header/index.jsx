a1bce7c89c1806946a74a68159264ccc37400837
Vitor Nunes <stecks10@gmail.com>
2023-04-24 16:47:18 -0300

    imagem do usuario

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header() {
   return (
     <Container>
-
+      <Profile>
+        <img
+          src='https://github.com/stecks10.png'
+          alt='Stecks10'
+        />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Vitor Nunes</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
