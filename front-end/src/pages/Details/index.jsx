0c83af32563485d7a8697b55f6127659dcfb6710
Vitor Nunes <stecks10@gmail.com>
2023-04-24 17:48:21 -0300

    component tag

--- front-end/src/pages/Details/index.jsx
+++ front-end/src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 import { Container, Links } from './styles';
 
 export function Details() {
@@ -13,6 +14,10 @@ export function Details() {
           <li><a href='#'>https://www.google.com.br</a></li>
           <li><a href='#'>https://www.google.com.br</a></li>
         </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="note" />
       </Section>
 
       <Button title="Voltar" />
