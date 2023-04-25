4ebcb0723956b686b4ee9057dc705dc0ed337bdc
Vitor Nunes <stecks10@gmail.com>
2023-04-25 12:12:53 -0300

    Avatar do Usuario

--- front-end/src/pages/Profile/index.jsx
+++ front-end/src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { Container, Form } from './styles';
+import { Container, Form, Avatar } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 export function Profile() {
   return (
@@ -13,6 +13,13 @@ export function Profile() {
       </header>
 
       <Form>
+        <Avatar>
+          <img src="https://github.com/stecks10.png" alt="Stecks" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
         <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="Email" type="text" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
