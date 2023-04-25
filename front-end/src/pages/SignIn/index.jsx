dea5a0ad69b1bef88fe221848b6e2e7d9ba183be
Vitor Nunes <stecks10@gmail.com>
2023-04-25 08:50:54 -0300

    Fomulario signin

--- front-end/src/pages/SignIn/index.jsx
+++ front-end/src/pages/SignIn/index.jsx
@@ -1,9 +1,26 @@
-import { Container } from './styles';
+import { FiMail, FiLock } from 'react-icons/fi';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { Container, Form } from './styles';
 
 export function SignIn() {
   return (
     <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links Úteis</p>
 
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="email" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          Criar conta
+        </a>
+      </Form>
     </Container>
   )
 }
