a426be378ceaeac687ae328d17e47c073f751f2c
Vitor Nunes <stecks10@gmail.com>
2023-04-25 17:40:55 -0300

    Rotas

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global'
-import { New } from './pages/New/'
-
+import { Routes } from './routes';
 import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
