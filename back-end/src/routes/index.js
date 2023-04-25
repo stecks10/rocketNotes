a2c5e01360b59a376e9c4f7cda85bde4f361c920
Vitor Nunes <stecks10@gmail.com>
2023-04-25 18:39:09 -0300

    Rotas de autenticaçao

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express');
 const usersRouter = require('./users.routes')
 const notesRoutes = require('./notes.routes')
 const tagsRoutes = require('./tags.routes')
+const sessionRoutes = require('./sessions.routes')
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionRoutes);
 routes.use('/notes', notesRoutes);
 routes.use('/tags', tagsRoutes);
 
