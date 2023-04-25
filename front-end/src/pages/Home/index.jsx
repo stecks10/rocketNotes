7f2c6921f6b4fff25f265919221ac26ed31bdc4f
Vitor Nunes <stecks10@gmail.com>
2023-04-25 08:03:20 -0300

    Input

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
 import { Header } from '../../components/Header';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiSearch } from 'react-icons/fi';
 import { ButtonText } from '../../components/ButtonText';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+import { Input } from '../../components/Input';
 
 export function Home() {
   return (
@@ -17,7 +18,7 @@ export function Home() {
 
       </Menu>
       <Search>
-
+        <Input placeholder="Pesquisar pelo titulo" icon={FiSearch} />
       </Search>
       <Content>
 
