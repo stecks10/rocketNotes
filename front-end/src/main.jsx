8b007b685359c257c9146d8d74fe0b07ab0a3b27
Vitor Nunes <stecks10@gmail.com>
2023-04-25 15:12:11 -0300

    interface new

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global'
-import { Profile } from './pages/Profile/'
+import { New } from './pages/New/'
 
 import theme from './styles/theme';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
