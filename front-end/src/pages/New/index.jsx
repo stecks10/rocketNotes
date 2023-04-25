5042534153df91246d604848ee8daab689da1856
Vitor Nunes <stecks10@gmail.com>
2023-04-25 15:15:43 -0300

    Header da new

--- front-end/src/pages/New/index.jsx
+++ front-end/src/pages/New/index.jsx
@@ -1,10 +1,23 @@
-import { Container } from './styles';
+import { Container, Form } from './styles';
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 
 export function New() {
   return (
     <Container>
       <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>
+              Criar Nota
+            </h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+        </Form>
+      </main>
     </Container>
   )
 }
