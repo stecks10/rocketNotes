3f06d65894135b09da0e0d50140c73b1d20d97f2
Vitor Nunes <stecks10@gmail.com>
2023-04-25 17:15:36 -0300

    estilizando o noteItem

--- front-end/src/pages/New/index.jsx
+++ front-end/src/pages/New/index.jsx
@@ -2,6 +2,10 @@ import { Container, Form } from './styles';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { TextArea } from '../../components/TextArea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+
+
 
 export function New() {
   return (
@@ -18,6 +22,12 @@ export function New() {
 
           <Input placeholder="Título" />
           <TextArea placeholder="Observações" />
+
+          <Section title="Links Úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+
+          </Section>
         </Form>
       </main>
     </Container>
