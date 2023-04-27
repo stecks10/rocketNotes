f0f79ef050efc287bdeed8ec707daf5277f9e9cc
Vitor Nunes <stecks10@gmail.com>
2023-04-27 11:32:40 -0300

    Carregando imagens

--- back-end/src/routes/users.routes.js
+++ back-end/src/routes/users.routes.js
@@ -1,12 +1,21 @@
 const { Router } = require('express');
 const UserController = require('../controllers/UsersController');
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const multer = require('multer');
+const uploadConfig = require('../configs/upload');
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const userController = new UserController();
 
 usersRoutes.post("/", userController.create);
 usersRoutes.put("/", ensureAuthenticated, userController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file.filename);
+    response.json({ ok: true });
+  }
+);
 
 module.exports = usersRoutes;
