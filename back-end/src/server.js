b7bef9c775f4f46a912fd34999b97d9752224207
Vitor Nunes <stecks10@gmail.com>
2023-04-28 08:10:16 -0300

    coros

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,14 +3,17 @@ require("express-async-errors");
 const AppError = require('./utils/AppError');
 const migrationsRun = require('./database/sqlite/migrations/index');
 const uploadConfig = require('./configs/upload');
+const cors = require('cors');
 
 const express = require('express');
 
 const routes = require('./routes')
 migrationsRun();
 
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
