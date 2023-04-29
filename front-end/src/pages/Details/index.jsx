40bf4dd5329ba46ea3e9882684068ca03f5e8feb
Vitor Nunes <stecks10@gmail.com>
2023-04-29 19:04:55 -0300

    remover nota

--- front-end/src/pages/Details/index.jsx
+++ front-end/src/pages/Details/index.jsx
@@ -18,6 +18,15 @@ export function Details() {
     navigate('/');
   }
 
+  function handleRemove() {
+    const confirm = window.confirm('Deseja mesmo excluir essa nota?');
+
+    if (confirm) {
+      api.delete(`/notes/${params.id}`);
+      navigate('/');
+    }
+  }
+
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`);
@@ -33,7 +42,7 @@ export function Details() {
         data && (
           <main>
             <Content>
-              <ButtonText title="Excluir nota" />
+              <ButtonText title="Excluir nota" onClick={handleRemove} />
 
               <h1>{data.title}</h1>
               <p>{data.description}</p>
