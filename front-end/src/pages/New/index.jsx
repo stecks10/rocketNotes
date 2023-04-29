76b50fca999ca9b1664fd8cb2332949669bc3609
Vitor Nunes <stecks10@gmail.com>
2023-04-29 16:32:30 -0300

    cadastrar nota

--- front-end/src/pages/New/index.jsx
+++ front-end/src/pages/New/index.jsx
@@ -7,12 +7,19 @@ import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
+import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 export function New() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState('');
 
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState('');
+
+  const navigate = useNavigate();
 
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink]);
@@ -32,6 +39,19 @@ export function New() {
     setTags(prevState => prevState.filter(tag => tag !== deleted));
   }
 
+  async function handleNewNote() {
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    })
+
+    alert("Nota criada com sucesso!")
+    navigate("/")
+  }
+
+
   return (
     <Container>
       <Header />
@@ -44,8 +64,14 @@ export function New() {
             <Link to="/">voltar</Link>
           </header>
 
-          <Input placeholder="Título" />
-          <TextArea placeholder="Observações" />
+          <Input
+            placeholder="Título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextArea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title="Links Úteis">
             {
@@ -75,8 +101,8 @@ export function New() {
               }
               <NoteItem
                 isNew
-                placeholder="Nova tag"
-                onChange={(e) => setNewTag(e.target.value)}
+                placeholder="Novo marcador"
+                onChange={e => setNewTag(e.target.value)}
                 value={newTag}
                 onClick={handleAddTag}
               />
@@ -84,7 +110,10 @@ export function New() {
 
           </Section>
 
-          <Button title="Salvar" />
+          <Button
+            title="Salvar"
+            onClick={handleNewNote}
+          />
         </Form>
       </main>
     </Container>
