9a3a1689f0f6bdc95be1c788a67f6d2fcc249c35
Vitor Nunes <stecks10@gmail.com>
2023-04-29 20:03:27 -0300

    Ajuste no avatar

--- front-end/src/pages/Profile/index.jsx
+++ front-end/src/pages/Profile/index.jsx
@@ -1,54 +1,53 @@
-import { useState } from 'react';
-import { useAuth } from '../../hooks/auth';
-import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
-import { Container, Form, Avatar } from './styles';
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
-import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
-import { api } from '../../services/api';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
 
+import { useAuth } from '../../hooks/auth'
+
+import { api } from '../../services/api'
+
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+import { Input } from './../../components/Input'
+import { Button } from './../../components/Button'
+import { Container, Form, Avatar } from "./styles"
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom'
 
 export function Profile() {
-  const { user, updateProfile } = useAuth();
+
+  const { user, updateProfile } = useAuth()
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
   const navigate = useNavigate();
 
-
-  const [name, setName] = useState(user.name);
-  const [email, setEmail] = useState(user.email);
-  const [passwordOld, setPasswordOld] = useState();
-  const [passwordNew, setPasswordOldNew] = useState();
-
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
-  const [avatar, setAvatar] = useState(avatarUrl);
-  const [avatarFile, setAvatarFile] = useState(null);
+  function handleBack() {
+    navigate(-1);
+  }
 
   async function handleUpdate() {
-    const user = {
+    const updated = {
       name,
       email,
       password: passwordNew,
       old_password: passwordOld
     }
+    const userUpdate = Object.assign(user, updated)
 
-    await updateProfile({
-      user,
-      avatarFile
-    })
-
-    navigate(-1)
+    await updateProfile({ user: userUpdate, avatarFile })
   }
 
   function handleChangeAvatar(event) {
-    const file = event.target.files[0];
-    setAvatarFile(file);
+    const file = event.target.files[0]
+    setAvatarFile(file)
 
-    const imagePreview = URL.createObjectURL(file);
-    setAvatar(imagePreview);
-  }
-
-  function handleBack() {
-    navigate(-1);
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
   }
 
   return (
@@ -60,6 +59,7 @@ export function Profile() {
       </header>
 
       <Form>
+
         <Avatar>
           <img src={avatar} alt="avatar" />
           <label htmlFor="avatar">
@@ -71,6 +71,7 @@ export function Profile() {
             />
           </label>
         </Avatar>
+
         <Input
           placeholder="Nome"
           type="text"
@@ -78,28 +79,33 @@ export function Profile() {
           value={name}
           onChange={e => setName(e.target.value)}
         />
+
         <Input
           placeholder="Email"
-          type="text"
+          type="email"
           icon={FiMail}
           value={email}
           onChange={e => setEmail(e.target.value)}
         />
+
         <Input
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
           onChange={e => setPasswordOld(e.target.value)}
         />
+
         <Input
-          placeholder="Nova Senha"
+          placeholder="Nova senha"
           type="password"
           icon={FiLock}
-          onChange={e => setPasswordOldNew(e.target.value)}
+          onChange={e => setPasswordNew(e.target.value)}
         />
+
         <Button title="Salvar" onClick={handleUpdate} />
+
       </Form>
+
     </Container>
   )
-
 }
